b2f3090c364c02a474322492f2ab160cac5c4d10
meapifulloption <mefulloption@gmail.com>
2024-12-05 13:51:01 +0700

    commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from slotmachine import *
+from slotsim.main import SlotMachine
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+from botsim.discord.bot import config
